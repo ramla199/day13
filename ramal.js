371bb211ba2ba5a21f5dfd692333b9bfd1641ce9
ramla199 <75176930+ramla199@users.noreply.github.com>
2022-02-21 23:09:02 +0000

    day-21

--- ramal.js
+++ ramal.js
@@ -94,3 +94,138 @@ var test = obj.prop.getFullname;
 console.log(test());
 
 console.log(test.call(obj.prop));
+
+const ramal = "hi";
+
+
+function reverse_a_number(n) {
+    n = n + "";
+
+    return n.split("").reverse().join("");
+}
+
+const Address = {
+    Australia: { Street: 'Victoria Rd', Suburb: 'Alberta', State: 'NSW', Zip: 2222 }
+}
+function getState(state) {
+    const { Australia: { State: stateName } } = state;
+    return stateName;
+}
+
+
+function reverse_a_number(n) {
+    n = n + "";
+
+    return n.split("").reverse().join("");
+}
+
+(function () {
+    var a = b = 5;
+})();
+
+console.log(b);
+
+(function () {
+    'use strict';
+    var a = window.b = 5;
+})();
+
+console.log(b);
+
+
+(function () {
+    var a = b = 5;
+})();
+
+console.log(b);
+
+(function () {
+    'use strict';
+    var a = window.b = 5;
+})();
+
+console.log(b);
+
+
+// repeat
+console.log('hello'.repeatify(3));
+
+
+String.prototype.repeatify = String.prototype.repeatify || function (times) {
+    var str = '';
+
+    for (var i = 0; i < times; i++) {
+        str += this;
+    }
+
+    return str;
+};
+
+function test() {
+    console.log(a);
+    console.log(foo());
+
+    var a = 1;
+    function foo() {
+        return 2;
+    }
+}
+
+test();
+
+function test() {
+    var a;
+    function foo() {
+        return 2;
+    }
+
+    console.log(a);
+    console.log(foo());
+
+    a = 1;
+}
+
+test();
+
+var fullname = 'John Doe';
+var obj = {
+    fullname: 'Colin Ihrig',
+    prop: {
+        fullname: 'Aurelio De Rosa',
+        getFullname: function () {
+            return this.fullname;
+        }
+    }
+};
+
+console.log(obj.prop.getFullname());
+
+var test = obj.prop.getFullname;
+
+console.log(test());
+
+console.log(test.call(obj.prop));
+
+const ramal = "hi";
+
+
+function reverse_a_number(n) {
+    n = n + "";
+
+    return n.split("").reverse().join("");
+}
+
+const Address = {
+    Australia: { Street: 'Victoria Rd', Suburb: 'Alberta', State: 'NSW', Zip: 2222 }
+}
+function getState(state) {
+    const { Australia: { State: stateName } } = state;
+    return stateName;
+}
+
+
+function reverse_a_number(n) {
+    n = n + "";
+
+    return n.split("").reverse().join("");
+}
