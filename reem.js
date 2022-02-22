50d62be560905d77d2e555b6af00795d9865aa37
ramla199 <75176930+ramla199@users.noreply.github.com>
2022-02-22 11:21:09 +0000

    day-22

---
+++ reem.js
@@ -0,0 +1,40 @@
+function reverse_a_number(n) {
+    n = n + "";
+
+    return n.split("").reverse().join("");
+}
+
+const Address = {
+    Australia: { Street: 'Victoria Rd', Suburb: 'Alberta', State: 'NSW', Zip: 2222 }
+}
+function getState(state) {
+    const { Australia: { State: stateName } } = state;
+    return stateName;
+}
+
+
+
+function test() {
+    var a;
+    function foo() {
+        return 2;
+    }
+
+    console.log(a);
+    console.log(foo());
+
+    a = 1;
+}
+
+console.log('hello'.repeatify(3));
+
+
+String.prototype.repeatify = String.prototype.repeatify || function (times) {
+    var str = '';
+
+    for (var i = 0; i < times; i++) {
+        str += this;
+    }
+
+    return str;
+};
