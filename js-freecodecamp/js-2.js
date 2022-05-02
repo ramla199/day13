d7713ecfef2ab8bcc4cb6e5ddad80f4e6b74369e
ramla199 <ramlush143@gmail.com>
2022-05-02 21:54:40 +0100

    day-2

---
+++ js-freecodecamp/js-2.js
@@ -0,0 +1,22 @@
+const mystery7 = 0 !== -0;
+
+console.log(mystery7);
+console.log(0 !== -0);
+
+console.log(undefined === undefined)
+console.log('12' != 12)
+console.log(15 <= 29)
+console.log('a' < 'b')
+console.log('a' > 'A');
+
+if (0) {
+    console.log('mystery 13');
+}
+
+const mystery14 = true && 7 < 10 && 3 + 5 === 2;
+console.log(mystery14)
+if (mystery14) {
+    console.log('mystery 14');
+}
+
+console.log(15 >= 19);
